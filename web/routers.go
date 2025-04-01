3e17fe836461006bcd0f491201b6f8ca73173c03
Mozhu <mozhu@gate.me>
2025-04-01 21:12:51 +0800

    🦄refactor(web): update

--- web/routers.go
+++ web/routers.go
@@ -15,16 +15,13 @@ type (
 
 type Routers []RegisterFunc
 
+func (rs *Routers) Register(f RegisterFunc) { *rs = append(*rs, f) }
 func (rs *Routers) Handle(method, path string, f gin.HandlerFunc) {
 	*rs = append(*rs, func(g *gin.RouterGroup) { g.Handle(method, path, f) })
 }
-
-func (rs *Routers) Register(f RegisterFunc) {
-	*rs = append(*rs, f)
-}
-
 func (rs Routers) Apply(g *gin.RouterGroup) {
 	for _, f := range rs {
 		f(g)
 	}
 }
+func Routes() []gin.RouteInfo { return engine.Routes() }
