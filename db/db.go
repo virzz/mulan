6033ce80454b3bc4945480293daa2e1af6149bbf
Mozhu <mozhu233@outlook.com>
2025-04-16 09:40:48 +0800

    ✨feat(db): add multi db

--- db/db.go
+++ db/db.go
@@ -1,11 +1,13 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"time"
 
+	cmap "github.com/orcaman/concurrent-map/v2"
 	"gorm.io/gorm"
 	gLogger "gorm.io/gorm/logger"
 
@@ -16,6 +18,7 @@ import (
 var (
 	std      *gorm.DB
 	oncePlus once.OncePlus
+	multi    cmap.ConcurrentMap[string, *gorm.DB]
 )
 
 func R() *gorm.DB {
@@ -27,16 +30,7 @@ func R() *gorm.DB {
 
 func Migrate(models ...any) error { return std.AutoMigrate(models...) }
 
-func Init(cfg *Config, force ...bool) error {
-	if len(force) > 0 && force[0] {
-		return connect(cfg)
-	}
-	return oncePlus.Do(func() (err error) {
-		return connect(cfg)
-	})
-}
-
-func connect(cfg *Config) (err error) {
+func connect(cfg *Config) (*gorm.DB, error) {
 	newLogger := gLogger.Default.LogMode(gLogger.Info)
 	if cfg.Debug {
 		newLogger = gLogger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), gLogger.Config{
@@ -70,12 +64,12 @@ func connect(cfg *Config) (err error) {
 		dialector = DialectorPgSQL(cfg)
 	}
 	// Open
-	std, err = gorm.Open(dialector, gormCfg)
+	db, err := gorm.Open(dialector, gormCfg)
 	if err != nil {
 		vlog.Error("Failed to connect db", "err", err.Error())
-		return err
+		return nil, err
 	}
-	sqlDB, err := std.DB()
+	sqlDB, err := db.DB()
 	if err != nil {
 		vlog.Warn("Failed to get sql.db", "err", err.Error())
 	} else {
@@ -84,7 +78,41 @@ func connect(cfg *Config) (err error) {
 		sqlDB.SetConnMaxLifetime(time.Duration(cfg.Conn.Lifetime) * time.Second) // 最大可复用时间
 	}
 	if cfg.Debug {
-		std = std.Debug()
+		db = db.Debug()
 	}
-	return nil
+	return db, nil
+}
+
+func Init(cfg *Config, force ...bool) error {
+	if len(force) > 0 && force[0] {
+		db, err := connect(cfg)
+		if err != nil {
+			return err
+		}
+		std = db
+		return nil
+	}
+	return oncePlus.Do(func() (err error) {
+		db, err := connect(cfg)
+		if err != nil {
+			return err
+		}
+		std = db
+		return nil
+	})
+}
+
+func New(cfg *Config, name string) (*gorm.DB, error) {
+	if name == "" {
+		name = cfg.Name
+	}
+	if multi.Has(name) {
+		return nil, fmt.Errorf("db %s already exists", name)
+	}
+	db, err := connect(cfg)
+	if err != nil {
+		return nil, err
+	}
+	multi.Set(name, db)
+	return db, nil
 }
