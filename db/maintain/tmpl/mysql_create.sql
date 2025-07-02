7ae4ab39a9a50d23d2cc4a6797632ff9d3ee2921
Mozhu <mozhu233@outlook.com>
2025-07-02 19:21:04 +0800

    🦄refactor(db): todo move to mulan-ext/db-*

--- db/maintain/tmpl/mysql_create.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER IF NOT EXISTS '{{.User}}'@'%' identified by '{{.Pass}}';
-CREATE DATABASE IF NOT EXISTS `{{.Name}}` CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_unicode_ci';
-GRANT ALL ON `{{.Name}}`.* to '{{.User}}'@'%';
-FLUSH PRIVILEGES;
